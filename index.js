94ba9b3ff7654eb58aa98313874d2b6a5ef4babe
MattH150197 <matt150197@gmail.com>
2023-01-27 21:39:26 +0000

    CSS

---
+++ index.js
@@ -0,0 +1,21 @@
+// const numbers = [1,2,3,4,5,6,7,8,9]
+
+// const squares = numbers.map((number,index)=>{
+//     return number * number
+// })
+
+// console.log("numbers", numbers)
+// console.log("squares", squares)
+
+// const evenNumbers = numbers.filter((number, index)=>{
+//     return % 2 == 0;
+// })
+
+// console.log("evenNumbers", evenNumbers)
+
+// const sum = numbers.reduce((previousValue,currentValue)=>{
+//     previousValue = previousValue + currentValue
+//     return previousValue
+// }, 0)
+
+// console.log("sum", sum)
